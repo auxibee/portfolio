a14806182b467bafd807ea93731d542076cad8e9
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-12-31 18:39:34 -0500

    add footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import hero from '@/app/logo.svg'
 
 export default function Home() {
+  const y = new Date()
+  const year = y.getFullYear()
   return (
         <>
           <nav>
@@ -22,10 +24,15 @@ export default function Home() {
             <div className='flex flex-wrap'>
 
             <div className='w-full md:w-1/2'>
-              <h3 className='text-2xl lg:text-4xl font-bold'>A self Taught developer with special Interest in 
-              <span className='text-violet-500'> Backend development</span>
-              </h3>
-              <div className='px-3 py-10'>
+              <h3 className='text-2xl lg:text-3xl font-bold'>Hi! I am Yaw Twumasi Acheampong.</h3>
+              <p className='py-3'>
+                A self taught fullstack developer with special interest in 
+                <span className='text-violet-500'> Backend development </span>
+                 using NodeJS technologies
+              </p>
+              
+              
+              <div className='py-10'>
                 <Link className='inline-block py-3 px-5 bg-violet-500 text-white font-bold uppercase' href='#'>Download Cv</Link>
               </div>
             </div>
@@ -56,8 +63,8 @@ export default function Home() {
           </section>
 
           <footer className='bg-violet-400' >
-            <div className='container mx-auto py-10 px-6'>
-            <h6 className='text-white font-bold'> &#169; 2023 </h6>
+            <div className='container mx-auto py-10 px-6 text-center'>
+            <h6 className='text-white font-bold'> Copyright &#169; {year} Auxibee | Yaw Twumasi Acheampong</h6>
             </div>
           </footer>
         </>
